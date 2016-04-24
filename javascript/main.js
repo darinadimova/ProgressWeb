c29767e04e7ef26da2913527c0fd16c1028ed2c4
darinadimova <rina.dimova@gmail.com>
2016-04-24 22:56:17 +0300

    added login form style added overlay and gif fixed search input style scroll to top added hr above main menu

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,15 @@
 function loginShow(){
 	document.getElementById('registerForm').style.display="none";
 	document.getElementById('loginForm').style.display="block";
-
+	document.getElementById('overlay').style.display="block";
 }
+
+document.addEventListener("scroll", function() {
+
+if (window.pageYOffset > 300){
+	document.getElementById('arrow').style.display = "block";
+};
+if (window.pageYOffset <100){
+	document.getElementById('arrow').style.display = "none";
+}
+});
