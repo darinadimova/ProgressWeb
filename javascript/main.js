e4ee03865b01493548d20e1fbd055158fd0f9727
darinadimova <rina.dimova@gmail.com>
2016-04-26 23:55:32 +0300

    finished register form style added to top button added hideform function

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,18 @@ function loginShow(){
 	document.getElementById('overlay').style.display="block";
 }
 
+function registerShow(){
+	document.getElementById('registerForm').style.display="block";
+	document.getElementById('loginForm').style.display="none";
+	document.getElementById('overlay').style.display="block";
+}
+
+function hideForm(){
+	document.getElementById('overlay').style.display="none";
+	document.getElementById('loginForm').style.display="none";
+	document.getElementById('registerForm').style.display="none";
+}
+
 document.addEventListener("scroll", function() {
 
 if (window.pageYOffset > 300){
