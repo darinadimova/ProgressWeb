019176ac58e6577755a703fbb7cbe0502309084c
darinadimova <rina.dimova@gmail.com>
2016-05-01 18:45:30 +0300

    added logo to be link to home changed place of search and account added dobrohrumvane.jpg in header added home page banner added forUs.html split css in 2 carousel.css and forUs.css

--- javascript/main.js
+++ javascript/main.js
@@ -1,41 +1,41 @@
-function loginShow(){
-	document.getElementById('registerForm').style.display="none";
-	document.getElementById('loginForm').style.display="block";
-	document.getElementById('forgottenPasswordForm').style.display="none";
-	document.getElementById('overlay').style.display="block";
+function loginShow() {
+    document.getElementById('registerForm').style.display = "none";
+    document.getElementById('loginForm').style.display = "block";
+    document.getElementById('forgottenPasswordForm').style.display = "none";
+    document.getElementById('overlay').style.display = "block";
 }
 
-function registerShow(){
-	document.getElementById('registerForm').style.display="block";
-	document.getElementById('loginForm').style.display="none";
-	document.getElementById('forgottenPasswordForm').style.display="none";
-	document.getElementById('overlay').style.display="block";
+function registerShow() {
+    document.getElementById('registerForm').style.display = "block";
+    document.getElementById('loginForm').style.display = "none";
+    document.getElementById('forgottenPasswordForm').style.display = "none";
+    document.getElementById('overlay').style.display = "block";
 }
 
-function forgottenPasswordShow(){
-	document.getElementById('registerForm').style.display="none";
-	document.getElementById('loginForm').style.display="none";
-	document.getElementById('forgottenPasswordForm').style.display="block";
-	document.getElementById('overlay').style.display="block";
+function forgottenPasswordShow() {
+    document.getElementById('registerForm').style.display = "none";
+    document.getElementById('loginForm').style.display = "none";
+    document.getElementById('forgottenPasswordForm').style.display = "block";
+    document.getElementById('overlay').style.display = "block";
 }
 
-function hideForm(){
-	document.getElementById('overlay').style.display="none";
-	document.getElementById('loginForm').style.display="none";
-	document.getElementById('registerForm').style.display="none";
-	document.getElementById('forgottenPasswordForm').style.display="none";
+function hideForm() {
+    document.getElementById('overlay').style.display = "none";
+    document.getElementById('loginForm').style.display = "none";
+    document.getElementById('registerForm').style.display = "none";
+    document.getElementById('forgottenPasswordForm').style.display = "none";
 }
 
-function msgSend(){
-	document.getElementById('forgottenPasswordForm').innerHTML="Съобщението беше изпратено усмешно!Нова парола беше изпратена на посочения емайл.";
+function msgSend() {
+    document.getElementById('forgottenPasswordForm').innerHTML = "Съобщението беше изпратено усмешно!Нова парола беше изпратена на посочения емайл.";
 }
 
 document.addEventListener("scroll", function() {
 
-if (window.pageYOffset > 300){
-	document.getElementById('arrow').style.display = "block";
-};
-if (window.pageYOffset <100){
-	document.getElementById('arrow').style.display = "none";
-}
+    if (window.pageYOffset > 300) {
+        document.getElementById('arrow').style.display = "block";
+    };
+    if (window.pageYOffset < 100) {
+        document.getElementById('arrow').style.display = "none";
+    }
 });
